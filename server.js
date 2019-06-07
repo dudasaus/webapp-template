8052bf3231f244e56444be075d4bd92b68483b77
Austin Dudas <dudasaus@umich.edu>
2019-06-06 22:53:28 -0400

    404 page

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 /**
  * Express server that statically serves a dsitrubtion directory
  */
+const path = require('path');
+const fs = require('fs');
 const express = require('express');
 
 // Distribution directory and default port
 const distribution = 'dist';
 const PORT = process.env.PORT || 8000;
+const notFoundHtml = path.join(__dirname, distribution, '404.html');
+
 
 // Start the server
 const app = express();
 app.use(express.static(distribution));
+// 404 Page if the static file wasn't found
+app.get('*', (req, res) => {
+  responseHtml = fs.readFileSync(notFoundHtml, { encoding: 'utf-8' });
+  res.status(404).send(responseHtml);
+});
+// Listen
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
